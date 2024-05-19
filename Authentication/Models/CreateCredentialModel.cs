46546ebc9b525b5ed3fa98ee8965b655e4633168
Marlon <34462179+Shonuvik@users.noreply.github.com>
2024-05-18 21:37:22 -0300

    Add files via upload

---
+++ Authentication/Models/CreateCredentialModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Authentication.Models
+{
+	public class CreateCredentialModel
+	{
+        public string UserName { get; set; }
+
+        public string ClientId { get; set; }
+
+        public string Secret { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
+
