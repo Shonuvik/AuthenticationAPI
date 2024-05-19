c97d710290560d5da9d41a3aa64cc3e135f5f32e
Marlon <34462179+Shonuvik@users.noreply.github.com>
2024-05-18 21:34:45 -0300

    ConnectionDatabase adjust

--- Authentication/Models/CredentialModel.cs
+++ Authentication/Models/CredentialModel.cs
@@ -1,10 +1,11 @@
-﻿namespace Authentication.Models
+namespace Authentication.Models
 {
     public class CredentialModel
 	{
+		public string UserName { get; set; }
+
 		public string ClientId { get; set; }
 
 		public string Secret { get; set; }
 	}
 }
-
